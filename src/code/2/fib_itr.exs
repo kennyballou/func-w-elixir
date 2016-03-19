295bf24053425eed651f69b1660c778d0efab62d
kballou <kballou@devnulllabs.io>
2016-03-18 23:18:38 -0600

    Misc code example updates
    Use more idiomatic examples

--- src/code/2/fib_itr.exs
+++ src/code/2/fib_itr.exs
@@ -1,14 +1,13 @@
 defmodule Fib do
   def seq(0), do: 0
   def seq(1), do: 1
-  def seq(n) when n > 1, do: compute_seq(n, 1, [0, 1])
+  def seq(n) when n > 1, do: compute_seq(n, 1, [1, 0])
 
   defp compute_seq(n, i, acc) when n == i do
-    Enum.at(acc, length(acc) - 1)
+    hd(acc)
   end
   defp compute_seq(n, i, acc) do
-    len = length(acc)
-    compute_seq(n, i + 1, acc++[Enum.at(acc, len-1) + Enum.at(acc, len-2)])
+    compute_seq(n, i+1, [Enum.at(acc, 0) + Enum.at(acc, 1) | acc])
   end
 end
 
--- src/code/2/qs.exs
+++ src/code/2/qs.exs
@@ -1,10 +1,13 @@
 defmodule Quicksort do
   def sort([]), do: []
   def sort([h|t]) do
-    lower = t |> Enum.filter(&(&1 <= h))
-    upper = t |> Enum.filter(&(&1 > h))
+    {lower, upper} = t |> Enum.partition(&(&1 <= h))
     sort(lower) ++ [h] ++ sort(upper)
   end
 end
 
-1..10 |> Enum.shuffle |> Quicksort.sort |> IO.inspect
+1..10
+  |> Enum.shuffle
+  |> IO.inspect
+  |> Quicksort.sort
+  |> IO.inspect
